da2af548a73581958be88ec2e2cb2346b6706247
Mickey <vickeylee.w@gmail.com>
2020-06-09 22:23:23 +1000

    Initail commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import Pages from './pages';
+import { InfoContext } from "../src/contexts/infoContext";
+
+function App() {
+  const { info } = useContext(InfoContext);
+
+  if (!info) {
+    return <div />;
+  }
+
+  return <Pages user={info} />;
+}
+
+export default App;
---
+++ src/contexts/infoContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useState, useEffect } from "react";
+export const InfoContext = createContext();
+
+const InfoContextProvider = props => {
+    const [info, setInfo] = useState(null);
+    useEffect(() => {
+        fetch('https://gitconnected.com/v1/portfolio/mickey7799')
+            .then(res => res.json())
+            .then(user => {
+                setInfo(user);
+            });
+    }, []);
+    return (
+        <InfoContext.Provider value={{info}}>
+            {props.children}
+        </InfoContext.Provider>
+    );
+};
+
+export default InfoContextProvider;
