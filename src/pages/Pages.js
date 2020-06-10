44368e1647cb7d08d85bbd212dc9d76adaa1252e
Mickey <vickeylee.w@gmail.com>
2020-06-10 20:40:08 +1000

    Add Router

---
+++ src/pages/Pages.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Me from './Me';
+import Projects from './Projects';
+import Work from './Work';
+import Education from './Education';
+import { InfoContext } from '../contexts/infoContext';
+
+const Pages = () => {
+
+    const { info } = useContext(InfoContext);
+    if (!info) {
+        return <Loader />;
+    }
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Me/>
+                </Route>
+                <Route path="/projects">
+                    <Projects/>
+                </Route>
+                <Route path="/work">
+                    <Work/>
+                </Route>
+                <Route path="/education">
+                    <Education/>
+                </Route>
+            </Switch>
+        </Router>
+
+    );
+
+};
+
+export default Pages;
